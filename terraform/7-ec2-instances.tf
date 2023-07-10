c6f93c269a749eeb5650e0414ff3bebeec3f222f
RahulTiple31 <111124427+RahulTiple31@users.noreply.github.com>
2023-07-09 21:21:36 -0700

    Update 7-ec2-instances.tf

--- terraform/7-ec2-instances.tf
+++ terraform/7-ec2-instances.tf
@@ -1,17 +1,1 @@
-resource "aws_instance" "instance_1" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
 
-  tags = {
-    Name = "instance_1"
-  }
-}
-
-resource "aws_instance" "instance_2" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "instance_2"
-  }
-}
