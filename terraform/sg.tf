3027ca74e548b476ce82d513de2eef74af1ee5f3
RahulTiple31 <111124427+RahulTiple31@users.noreply.github.com>
2023-07-16 05:54:49 -0700

    Update sg.tf

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -3,12 +3,16 @@ resource "aws_security_group" "allow_tls" {
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  dynamic "ingress" {
+    for_each = [80, 22, 443, 8085]
+    iterator = port
+    content {
+      description = "TLS from VPC"
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
   }
 
   egress {
